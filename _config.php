af7889a493d03ec73ef0ed40b4ef0f317cc666d0
Petar Simic <petar.simic@fractas.com>
2018-01-14 14:29:00 +0100

    remove config that interfere with standard SilverStripe TInyMCE config

--- _config.php
+++ _config.php
@@ -7,10 +7,3 @@ use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 HtmlEditorConfig::get('cms')->enablePlugins('template');
 HtmlEditorConfig::get('cms')->setOptions(array('templates' => CannedContentController::FetchTemplates()));
 HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'template');
-
-HtmlEditorConfig::get('cms')->enablePlugins('searchreplace');
-HtmlEditorConfig::get('cms')->insertButtonsAfter('find', 'replace');
-
-HtmlEditorConfig::get('cms')->setOption('convert_fonts_to_spans', false);
-HtmlEditorConfig::get('cms')->setOption('valid_elements','*');
-HtmlEditorConfig::get('cms')->setOption('extended_valid_elements','*');
